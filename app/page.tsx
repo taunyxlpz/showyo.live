8487b6a956c10e17fe79897e0a274113f5b808dc
Manny X <taunyxlpz@gmail.com>
2025-08-30 03:44:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,10 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
 
-import SafeImage from './components/SafeImage';
-
 export default function Page() {
   return (
     <main className="p-6">
-      <SafeImage
-        src="/ShowYo Yellow.png"
-        alt="ShowYo"
-        width={240}
-        height={80}
-        className="h-auto w-auto"
-      />
+      {/* Home content */}
     </main>
   );
 }
