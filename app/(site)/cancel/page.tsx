57ba5d5755f0f7a53896f7bd97be454aa6122fee
Manny X <taunyxlpz@gmail.com>
2025-08-30 02:02:35 -0400

    Create page.tsx

---
+++ app/(site)/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function Cancel() {
+  return (
+    <div className="container">
+      <h1>Payment canceled</h1>
+      <p className="muted">No charge was made. You can try again anytime.</p>
+    </div>
+  );
+}
