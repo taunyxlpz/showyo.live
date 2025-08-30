ad38d04ac234bc15e3173127a87a7c59c4a50bf2
Manny X <taunyxlpz@gmail.com>
2025-08-30 04:12:15 -0400

    Update BrandLogo.tsx

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function BrandLogo({ className = '' }: { className?: string }) {
   return (
     <Image
-      src="/showyo-yellow.png"   // ← lives in /public
+      src="/showyo-yellow.png"   // lives in /public
       alt="ShowYo"
       width={240}
       height={80}
