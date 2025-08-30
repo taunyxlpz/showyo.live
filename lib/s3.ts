9aa7d6af2ab98d89ad0f514b624fa5b05d51d647
Manny X <taunyxlpz@gmail.com>
2025-08-30 02:17:56 -0400

    Update s3.ts

--- lib/s3.ts
+++ lib/s3.ts
@@ -8,8 +8,8 @@ export const s3 = new S3Client({
   region: REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-  }
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
 });
 
 export async function presign(key: string, expiresIn = 300) {
