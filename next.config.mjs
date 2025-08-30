5b5e9b8f82149435690adf1debf18ebb3823b5a3
Manny X <taunyxlpz@gmail.com>
2025-08-30 01:59:30 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  headers: async () => ([
+    { source: '/player', headers: [{ key: 'Cache-Control', value: 'no-store' }] },
+    { source: '/api/(.*)', headers: [{ key: 'Cache-Control', value: 'no-store' }] }
+  ])
+};
+
+export default nextConfig;
